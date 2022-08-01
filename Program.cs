3d48c4d282b0fa43ff1126f51b0e708ab4a861b1
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2022-08-01 18:36:48 -0300

    Intervalo2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Exercício do site URI "intervalo2"
+
+int X, Dentro, Fora;
+int N = int.Parse(Console.ReadLine());
+
+Dentro = 0;
+Fora = 0;
+for (int i = 1; i <= N; i++)
+{
+    X = int.Parse(Console.ReadLine());
+    if (X >= 10 && X <= 20)
+    {
+        Dentro ++;
+    }
+    else
+    {
+        Fora ++;
+    }
+}   
+Console.WriteLine(Dentro + " in");
+Console.WriteLine(Fora + " out");
